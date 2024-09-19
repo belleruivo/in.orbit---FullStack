050ef1ecd4752aaf34f3a324c16f6936e064de5c
Isabelle Ruivo dos Santos <isabelleruivosantos@gmail.com>
2024-09-19 15:31:12 -0300

    InOrbit full-stack app

---
+++ server/src/http/server.ts
@@ -0,0 +1,34 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>() // criando a aplicação usando app = fastify
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    // o listen é uma promisse que vai ficar ouvindo a porta 3333, que pode demorar um pouco pra executar
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server runing!')
+  })
